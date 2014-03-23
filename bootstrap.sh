39e827cab7dd1c031d0482c46362dfca754f91ad
Achim Abeling <achimabeling@web.de>
2014-03-23 11:15:21 +0100

    apache needs a restart after package installation to make mysql support available

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ echo "this is the bootstrap installer"
 
 # install packages
 apt-get update
-apt-get install -y apache2 php5 unzip php5-mysql
+apt-get install -y php5 php5-mysql unzip php5-mysql apache2
 
 # get joomla 1.5
 mkdir -p install
@@ -13,6 +13,8 @@ wget http://joomlacode.org/gf/download/frsrelease/16890/73390/Joomla_1.5.26-Stab
 unzip Joomla_1.5.26-Stable-Full_Package.zip
 cp -ru * /var/www/
 chown -R www-data.www-data /var/www
+
+/etc/init.d/apache2 restart
 
 echo "joomla base installation is now available under http://localhost:8080/index.php"
 
